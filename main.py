11234e60dfd15c2a990277f3d850b59948595011
Kamil Dydo <dydokamil@gmail.com>
2017-08-08 15:44:43 +0200

    Stemming, stopwords removal, vectorization, prediction

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+"""Dataset taken from http://spamassassin.apache.org/old/publiccorpus/"""
+
 import email
 import os
 import re
 from email.message import Message
 
 import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import cross_val_score, train_test_split
 
 DATA_PATH = '/media/hdd/training_data/spamham'
 dirs = os.listdir(DATA_PATH)
@@ -53,6 +58,25 @@ def cleanup(messages):
     # remove empty messages
     messages = [message for message in messages if message]
 
+    # turn messages into strings again
+    messages = [' '.join(message) for message in messages]
+
+    return messages
+
+
+def stem(messages):
+    from nltk.stem.snowball import SnowballStemmer
+    stemmer = SnowballStemmer('english')
+    messages = [[stemmer.stem(word) for word in words.split(' ') if word is not ''] for words in messages]
+    return messages
+
+
+def remove_stopwords(messages):
+    from nltk.corpus import stopwords
+    stopwords = set(stopwords.words('english'))
+    messages = [[word for word in words if word not in stopwords] for words in messages]
+    # turn messages into strings again
+    messages = [' '.join(message) for message in messages]
     return messages
 
 
@@ -69,5 +93,34 @@ for dir in dirs:
 X_spam = cleanup(spam_messages)
 X_ham = cleanup(ham_messages)
 
-y = np.concatenate([np.zeros(len(ham_messages)), np.ones(len(spam_messages))])
-X = spam_messages + ham_messages
+X_spam = stem(X_spam)
+X_ham = stem(X_ham)
+
+X_spam = remove_stopwords(X_spam)
+X_ham = remove_stopwords(X_ham)
+
+y = np.concatenate([np.zeros(len(X_spam)), np.ones(len(X_ham))])
+# X = np.concatenate([X_spam, X_ham])
+X = X_spam + X_ham
+
+assert len(X) == len(y)
+
+# split into train/test subsets
+X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+# CountVectorizer
+cv = CountVectorizer(min_df=0.005, analyzer='word', ngram_range=(1, 3))
+X_train = cv.fit_transform(X_train)
+X_test = cv.transform(X_test)
+
+# StratifiedKFold()
+
+tfidf_transformer = TfidfTransformer(norm='l1', use_idf=True)
+X_train = tfidf_transformer.fit_transform(X_train)
+X_test = tfidf_transformer.transform(X_test)
+
+clf = RandomForestClassifier(30)
+clf.fit(X_train, y_train)
+
+print(cross_val_score(clf, X_test, y_test, scoring='accuracy'))
+print(clf.score(X_test, y_test))
